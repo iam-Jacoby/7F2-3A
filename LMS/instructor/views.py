411805bcd70e2c5dc72097c80881a66e56571f23
theDAOS <sanjusanthosh46@gmail.com>
2024-08-06 22:12:05 +0530

    Added Template for instructor and added all app to installed apps in settings

--- LMS/instructor/views.py
+++ LMS/instructor/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def instructor_dashboard(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('dashboard.html')
+    return HttpResponse(template.render())
