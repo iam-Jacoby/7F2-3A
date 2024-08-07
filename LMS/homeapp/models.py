e05bcdeb1249e88f94796d62a87eb0fda52f4a88
theDAOS <sanjusanthosh46@gmail.com>
2024-08-07 16:22:06 +0530

    Error Fixed

--- LMS/homeapp/models.py
+++ LMS/homeapp/models.py
@@ -10,4 +10,4 @@ class User(models.Model):
     user_id = models.BigAutoField(primary_key=True)
     email = models.EmailField(max_length=254)
     name = models.CharField(max_length=60)
-    user_type = models.models.CharField(max_length=2, choices=account_type)
+    user_type = models.CharField(max_length=2, choices=account_type)
