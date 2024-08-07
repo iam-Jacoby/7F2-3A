0b2a2412c48250579ee3b65eca561cadbc68c163
theDAOS <sanjusanthosh46@gmail.com>
2024-08-07 13:35:05 +0530

    Cleaned Navbar added add button, added session (userid temp)

--- LMS/instructor/views.py
+++ LMS/instructor/views.py
@@ -4,4 +4,15 @@ from django.template import loader
 
 def instructor_dashboard(request):
     template = loader.get_template('dashboard.html')
-    return HttpResponse(template.render())
+
+    request.session["userid"] = 100
+
+    userid = request.session.get("userid")
+    print(userid)
+
+
+    context = {
+        'userid' : userid,
+    }
+
+    return HttpResponse(template.render(context, request))
