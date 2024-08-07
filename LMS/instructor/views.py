41370bee7858958af0f3c8807f8732585fb7e551
theDAOS <sanjusanthosh46@gmail.com>
2024-08-07 16:44:55 +0530

    Added a temp user details

--- LMS/instructor/views.py
+++ LMS/instructor/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from homeapp.models import User
 
 def instructor_dashboard(request):
     template = loader.get_template('dashboard.html')
 
-    request.session["userid"] = 100
+    request.session["userid"] = 1
 
     userid = request.session.get("userid")
 
+    user_details = User.objects.get(user_id=userid)
+
     context = {
         'userid' : userid,
+        "user_details" : user_details,
     }
 
     return HttpResponse(template.render(context, request))
