a5914f91ae02cec7a6f95827912ab2ae9314ebca
theDAOS <sanjusanthosh46@gmail.com>
2024-09-29 11:34:28 +0530

    User Registration Django rest succese

--- LMS/homeapp/urls.py
+++ LMS/homeapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import UserRegistrationView
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('register/', UserRegistrationView.as_view(), name='user-register'),
 ]
--- LMS/homeapp/views.py
+++ LMS/homeapp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import UserRegistrationSerializer
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
 def home(request):
     return HttpResponse("Hello world!")
