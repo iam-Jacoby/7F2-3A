d52c928da7e8506c20f1aeba4b6d5fcc9897caa7
theDAOS <sanjusanthosh46@gmail.com>
2024-09-30 11:55:19 +0530

    logout alldone

--- LMS/homeapp/views.py
+++ LMS/homeapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, alogin
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.contrib import messages
 from .forms import UserRegisterForm, UserLoginForm
 
+def logoutView(request):
+    logout(request)
+    return redirect('login')
 
 def loginView(request):
     if request.method == 'POST':
@@ -13,7 +16,6 @@ def loginView(request):
             password = form.cleaned_data['password']
             user = authenticate(request, username=username, password=password)
             if user is not None:
-                # alogin(request, user)
                 login(request, user)
                 return redirect('home')
             else:
