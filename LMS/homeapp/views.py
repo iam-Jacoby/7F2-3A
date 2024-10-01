da5cd6bfd795c37343a2b310ef493960c2eda90e
theDAOS <sanjusanthosh46@gmail.com>
2024-10-01 13:21:16 +0530

    userSplitterView added

--- LMS/homeapp/views.py
+++ LMS/homeapp/views.py
@@ -6,18 +6,20 @@ from django.contrib import messages
 from .forms import UserRegisterForm, UserLoginForm
 from .models import User_types
 
+@login_required(login_url="login/")
 def userSplitterView(request):
     user_id = request.user.id
-    user_type = get_object_or_404(User_types, user_id=user_id)
-    if user_type.user_type ==  'STU':
+    user_type = User_types.objects.get(user_id=user_id)
+    if user_type.user_type ==  'INS':
         return redirect('instructor_dashboard')
-    elif user_type.user_type ==  'INS':
+    elif user_type.user_type ==  'STU':
         return redirect('home')
     elif user_type.user_type ==  'ADM':
         return redirect('home')
     else:
         return HttpResponse("Error user_type={user_type}, user_type.user_type={user_type.user_type}")
 
+@login_required(login_url="login/")
 def logoutView(request):
     logout(request)
     return redirect('login')
@@ -31,14 +33,13 @@ def loginView(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return redirect('userSplitterView')
             else:
                 messages.add_message(request, messages.ERROR, "Invalid username or password")
     else:
         form = UserLoginForm()
     
     return render(request, 'login.html', {'form': form})
-
 
 def register(request):
     if request.method == 'POST':
@@ -47,7 +48,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('home')
+            return redirect('userSplitterView')
     else:
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
