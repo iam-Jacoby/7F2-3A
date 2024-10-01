9ee740cc008ba8d2842a60ded71b96cc3d733764
theDAOS <sanjusanthosh46@gmail.com>
2024-10-01 09:24:27 +0530

    User_types model created

--- LMS/homeapp/models.py
+++ LMS/homeapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class User_types(models.Model):
+    USER_TYPE_CHOICES = [
+        ('STU', 'Student'),
+        ('INS', 'Instructor'),
+        ('ADM', 'Admin'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=3, choices=USER_TYPE_CHOICES)
